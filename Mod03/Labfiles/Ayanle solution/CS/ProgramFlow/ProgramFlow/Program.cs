04bb11fe5a9803cbf5a6004084a1dd45d63193b2
ayanlea <ayanle@selfbook.com>
2022-10-04 13:55:29 -0700

    Exercise 1

---
+++ Mod03/Labfiles/Ayanle solution/CS/ProgramFlow/ProgramFlow/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 2:
+            SetInstructorName("Ayanle");
+            SetCourseTitle("Intro to Existentialism!");
+            ValidateCourseTitle("Intro to Existentialism!");
+            Console.ReadKey();
+        }
+
+        //Task 1:
+        static void SetInstructorName(string name)
+        {
+            Console.WriteLine("Instructor name is set to " + name);
+        }
+        static void SetCourseTitle(string courseName)
+        {
+            Console.WriteLine("Course name is " + courseName);
+        }
+
+        //Task 3:
+        private static bool ValidateCourseTitle(string title)
+        {
+            if(title == "Intro to Existentialism!")
+            {
+                return true;
+            }
+            else { 
+                return false; 
+            }
+
+        }
+        static void CalculateAverage()
+        {
+            double[] grades = new double[] { 89, 98, 99, 90, 95 };
+            
+
+        }
+    }
+}

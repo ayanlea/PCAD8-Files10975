0adbb7542f21a7d995111780d1e8257f663fb839
ayanlea <ayanle@selfbook.com>
2022-10-05 11:58:14 -0700

    Ex 2, Mod04

--- Mod04/Labfiles/Ayanle solution/CS/AlgorithmDataStructures_CS/AlgorithmDataStructures_CS/Program.cs
+++ Mod04/Labfiles/Ayanle solution/CS/AlgorithmDataStructures_CS/AlgorithmDataStructures_CS/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,39 +12,39 @@ namespace AlgorithmDataStructures_CS
     {
         static void Main(string[] args)
         {
-            // Create an array here called grdsArray
+
+            
+
+            // Create an array here called grdsArray with these values: 80, 90, 100, 95, 85
+            float[] grdsArray = new float[] { 80, 90, 100, 95, 85 };
+
+            // Call the addGrades method, passing it the grdsArray
+            addGrades(grdsArray);
+            
+            // After adding grades to the array, call the displayGrades method
+            displayGrades(grdsArray);
+
+
+            // Create a new Stack object called myStack
+            Stack myStack = new Stack();
+
+            // Call the pushStack() method passing in the grades array for values
+
+            pushStack(grdsArray, myStack);
+
+            // Thinking through the process of viewing and choosing courses, write the pseudocode steps that you would complete for reviewing and choosing courses.
 
             
 
 
-            // Call the addGrades method, passing it the grdsArray
 
-
-
-
-            // After adding grades to the array, call the displayGrades method
-            // to print out the grades to the console window
-            // Use the foreach construct to iterate over the array
-
-
-
-
-            // Create a new Stack object called myStack
-
-
-
-
-            // Call the pushStack() method passing in the grades array for values
-
-
-
-
-            // Call the popStack() method twice to remove the top two items from stack
             // The popStack method will display each popped item to the console window
-
-
-
-
+            // Then call the popStack() method twice to remove the top two items from stack
+            Console.WriteLine("Peek at item to be popped from Stack", myStack.Peek());
+            myStack.Pop();
+            Console.WriteLine("Peek at item to be popped from Stack", myStack.Peek());
+            myStack.Pop();
+            
             // Create a new SortedList object called myCourses
 
 
@@ -64,19 +65,39 @@ namespace AlgorithmDataStructures_CS
         }
 
 
+
         static void addGrades(float[] grdArray)
         {
-            
+            double total = 0;
+            double average = 0;
+            for (int i = 0; i < grdArray.Length; i++)
+            {
+                total += grdArray[i];
+            }
+            average = total / grdArray.Length;
+            Console.WriteLine("The average grade is {0}", average);
+
         }
 
         static void displayGrades(float[] grdArray)
         {
-            
+            // print out the grades to the console window
+            // Use the foreach construct to iterate over the array
+            foreach (float grade in grdArray)
+            {
+                Console.WriteLine("Grade: " + grade);
+            }
+
         }
 
-
-        static void pushStack(float[] grdArray)
+        static void pushStack(float[] grdArray,Stack myStack)
         {
+
+            // Use a foreach loop to iterate over the array and push each item onto the stack
+            foreach (float grade in grdArray)
+            {
+                myStack.Push(grade);
+            }
 
         }
 

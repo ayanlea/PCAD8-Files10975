710f11e9bbf75243c7c3723804779e6df72dc352
ayanlea <ayanle@selfbook.com>
2022-10-12 10:20:50 -0700

    Mod09

---
+++ Mod09/Labfiles/Ayanle-Solution/CS/Console_IO_CS/Console_IO_CS/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_IO_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuPrompt();
+
+            char response;
+
+            response = Convert.ToChar(Console.Read());
+
+            if (response != null)
+            {
+                UserSelection(response);
+            }
+        }
+
+        private static void MenuPrompt()
+        {
+            Console.WriteLine("Do you want to create a:");
+
+            Console.WriteLine("S - Student");
+            Console.WriteLine("I - Instructor");
+            Console.WriteLine("Q - Quit");
+        }
+        private static void UserSelection(char response)
+        {
+            if(response == 'S')
+            {
+                Console.WriteLine("You have selected Student");
+            }
+            else if (response == 'I')
+            {
+                Console.WriteLine("You have selected Instructor");
+            }
+            else if (response == 'Q')
+            {
+                Console.WriteLine("Are you sure you want to quit?");
+                Console.WriteLine("Y - Yes");
+                Console.WriteLine("N - No");
+                while(response != 'Y' || response != 'N')
+                {
+                    char localRes;
+                    localRes = Convert.ToChar(Console.Read());
+                    if (localRes == 'Y')
+                    {
+                        Console.WriteLine("Shutting down application");
+                    }
+                    else if (localRes == 'N')
+                    {
+                        Console.WriteLine("Resuming application");
+                        Console.WriteLine();
+                        MenuPrompt();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Mod09/Labfiles/Ayanle-Solution/CS/File_IO_CS/File_IO_CS/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_IO_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MenuPrompt();
+
+            char response;
+
+            response = Convert.ToChar(Console.Read());
+
+            if (response != null)
+            {
+                UserSelection(response);
+            }
+        }
+        private static void MenuPrompt()
+        {
+            Console.WriteLine("Do you want to create a:");
+
+            Console.WriteLine("S - Student");
+            Console.WriteLine("I - Instructor");
+            Console.WriteLine("Q - Quit");
+        }
+        private static void UserSelection(char response)
+        {
+            if (response == 'S' || response == 's')
+            {
+                Console.WriteLine("You have selected Student");
+            }
+            else if (response == 'I' || response == 'i')
+            {
+                Console.WriteLine("You have selected Instructor");
+            }
+            else if (response == 'Q' || response == 'q')
+            {
+                Console.WriteLine("Are you sure you want to quit?");
+                Console.WriteLine("Y - Yes");
+                Console.WriteLine("N - No");
+                bool looper = true;
+                while (looper)
+                {
+                    char localRes;
+                    localRes = Convert.ToChar(Console.Read());
+                    if (localRes == 'Y' || localRes == 'y')
+                    {
+                        Console.WriteLine("Shutting down application");
+                        looper = false;
+                    }
+                    else if (localRes == 'N' || localRes == 'n')
+                    {
+                        Console.WriteLine("Resuming application");
+                        Console.WriteLine();
+                        MenuPrompt();
+                    }
+                }
+            }
+        }
+    }
+}
